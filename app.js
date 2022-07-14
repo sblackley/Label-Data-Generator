62ab0fa9ab20adb50d735262d7d96fee1ec4f045
sblackley <sblackey1@gmail.com>
2022-07-14 12:53:06 -0400

    Cleaned up logic and added comments

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ let textValue = "";
 let textArray = [];
 let splitArray = [];
 let objArray = [];
-const PREFIX = "RESET";
 
 
+// Define object with properties corresponding to requred label fields
 class Data {
     constructor(name, desc, startDate, endDate) {
         this.name = name;
@@ -19,7 +19,6 @@ function getText() {
     // Clear any remaining paragraphs in output area
     document.getElementById('outputArea').textContent = "";
     
-    // 
     if (document.getElementById('textInput').value != null) {
         textValue = document.getElementById('textInput').value;
         console.log(textValue);
@@ -37,12 +36,17 @@ function getText() {
 }
 
 function search() {
+    // Get search terms from text box
     let querytest = document.getElementById('numberInput').value;
+
+    // Split into array
     let queryArr = querytest.split("\n");
 
+    // For each search term, compare it to each reset number in the CSV array
     for (let i = 0; i < queryArr.length; i++) {
         for (let j = 0; j < splitArray.length; j++) {
-            
+
+            // If the reset in the CSV array contains the search number, create an object with the label properties and add to an array of those objects
             if (splitArray[j][2].includes(queryArr[i])) {
                 objArray.push(new Data(splitArray[j][2].replaceAll('"', ''), splitArray[j][1].replaceAll('"', ''), splitArray[j][9].replaceAll('"', ''), splitArray[j][10].replaceAll('"', '')));   
             }
@@ -53,8 +57,9 @@ function search() {
     displayData();
 }
 
+// Prints the contents of the object array 
 function displayData() {
-
+    // First change the date format
     changeDateFormat();
 
     objArray.forEach(element => {
@@ -64,6 +69,7 @@ function displayData() {
         console.log(element.endDate);
     });
 
+    // For each object, create a paragraph element populated with the data needed for the reset labels
     objArray.forEach(element => {
         const node = document.createElement("p");
         node.innerHTML = 
@@ -74,10 +80,9 @@ function displayData() {
         document.getElementById('outputArea').appendChild(node);
     }); 
     
+    // Reset all arrays after search is performed to prevent duplicates on subsequent runs
     resetData();
 }
-
-
 
 
 function resetData() {
@@ -86,7 +91,7 @@ function resetData() {
     objArray = [];
 }
 
-// Check this out, this is going to be real stupid
+// Changes date format from YYYY-MM-DD to MM-DD-YYYY
 function changeDateFormat() {
 
     let newStartDateArr = [];
@@ -96,16 +101,19 @@ function changeDateFormat() {
     let splitStart = [];
     let splitEnd = [];
 
+    // Populate arrays with the start and end date properties for each object
     objArray.forEach(element => {
         newStartDateArr.push(element.startDate);
         newEndDateArr.push(element.endDate);
     });
 
+    // Split dates into array in the form [[YYYY,MM,DD], [YYYY-MM-DD]]
     for (let i = 0; i < objArray.length; i++) {
         splitStart.push(newStartDateArr[i].split('-'));
         splitEnd.push(newEndDateArr[i].split('-'));
     }
 
+    // Move the year to the end of the array
     for (let i = 0; i < objArray.length; i++) {
         temp = splitStart[i].shift();
         let tempTrim = temp.trim();
@@ -119,12 +127,13 @@ function changeDateFormat() {
     console.log(splitStart);
     console.log(splitEnd);
 
+    // Rejoin subarrays in the form MM-DD-YYYY
     for (let i = 0; i < objArray.length; i++) {
         newStartDateArr[i] = splitStart[i].join("-");
         newEndDateArr[i] = splitEnd[i].join("-")    
     }
 
-
+    // Update each object with the new dates in the correct format
     for (let i = 0; i < objArray.length; i++) {
         objArray[i].startDate = newStartDateArr[i];
         objArray[i].endDate = newEndDateArr[i];
@@ -137,9 +146,3 @@ function changeDateFormat() {
 }
 
 // TODO: Add message to display reset numbers that weren't found
-
-
-// At this point all of the found numbers have been loaded into the array as objects with the required 
-// label fields as properties. The next step is to create HTML elements to display the data from those objects. 
-// Research and try: Element#after
-// 
